551a342efdd0530f73e72210f40234b3fe9a3d3a
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-08-20 16:31:21 +0530

    style added to input and button

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -3,17 +3,80 @@ import { Column, Row } from "./Layout";
 import styled from "styled-components";
 
 const Button = styled.button`
-  padding: 5px;
-  border: 1px sold #ccc;
-  border-radius: 4px;
-  margin-top: 2px;
-  background-color: ${(props) => (props.primary ? "blue" : "yellow")};
+  display: inline-block;
+  font-weight: 400;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  user-select: none;
+  padding:5px;
+  font-size: .9rem;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
   color: #fff;
+  border: 1px solid transparent;
+
+  &:focus {
+    outline: 0;
+    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+  }
+
+  &:hover {
+    text-decoration: none;
+  }
+
+  &:disabled {
+    opacity: 0.65;
+    box-shadow: none;
+  }
+
+  ${(props) =>
+    props.primary &&
+    `
+      background-color: #007bff;
+      &:hover {
+        background-color: #0056b3;
+      }
+    `}
+
+  ${(props) =>
+    props.secondary &&
+    `
+      color: #383d41;
+      background-color: #ffd500;
+      border-color: #ffd500;
+
+      &:hover {
+        color: #212529;
+        background-color: #ffcc00;
+        border-color: #ffc200;
+      }
+    `}
 `;
 
 const Input = styled.input`
+  display: block;
+  width: 100%;
   padding: 2px;
-  border-radius: 4px;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  &:focus {
+    color: #495057;
+    background-color: #fff;
+    border-color: #80bdff;
+    outline: 0;
+    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+  }
 `;
 
 const TaskForm = ({ handleAddTodos, handleError }) => {
@@ -43,6 +106,7 @@ const TaskForm = ({ handleAddTodos, handleError }) => {
         <Button primary onClick={handleSubmit}>
           Add Task
         </Button>
+        <Button secondary>Secondary Button</Button>
       </Column>
     </Column>
   );
